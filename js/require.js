51ed577d02c57b9db60d2df7d8d63c22da4622ca
webgeek2021 <harshangmak01@gmail.com>
2023-08-06 17:44:04 +0530

    made some changes in css and html

--- js/require.js
+++ js/require.js
@@ -1,32 +1,118 @@
 gsap.registerPlugin(ScrollTrigger);
 
+const menubtn = document.getElementById("menubtn");
+const backToTopBtn = document.getElementById("back-to-top-btn");
+const contactForm = document.getElementById("contactForm");
+const ToastContainer = document.getElementById("toast-container");
+
+window.addEventListener("scroll", () => {
+  const scrollY = window.scrollY;
+  const visibleThreshold = 400;
+
+  if (scrollY > visibleThreshold) {
+    backToTopBtn.style.display = "block";
+  } else {
+    backToTopBtn.style.display = "none";
+  }
+});
+
+menubtn.addEventListener("click", () => {
+  const options = document.getElementById("option-li");
+
+  if (options.classList.contains("show-options")) {
+    options.classList.remove("show-options");
+    menubtn.classList.remove("animate-humberger");
+  } else {
+    menubtn.classList.add("animate-humberger");
+    options.classList.add("show-options");
+  }
+});
+
+backToTopBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+
+if(contactForm){
+contactForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const name = document.getElementById("name");
+  const email = document.getElementById("email");
+  const phone = document.getElementById("phone");
+  const service = document.getElementById("service");
+  const message = document.getElementById("message");
+
+  // console.log(name, email, phone, service, message);
+
+  let Data = new FormData();
+  Data.append("Name", name.value);
+  Data.append("Email", email.value);
+  Data.append("Phone", phone.value);
+  Data.append("ServiceType", service.value);
+  Data.append("Message", message.value);
+  Data.append("Date", new Date());
+
+  fetch(
+    "https://script.google.com/macros/s/AKfycbxz5QbIT2Sfj3ujeJ6wGc8jshB7XvQMdnQlTnTeC0Ca1Y2mjbcFjPRx4HVMXHuJteBO/exec",
+    {
+      method: "POST",
+      body: Data,
+    }
+  ).then((res) => {
+    if (res.ok) {
+      name.value = "";
+      email.value = "";
+      phone.value = "";
+      service.value = "Repair_Service";
+      message.value = "";
+
+      ToastContainer.style.display = "block";
+      setTimeout(() => {
+        ToastContainer.style.display = "none";
+      }, 3000);
+    }
+  });
+});
+
+}
+
 const scrubValue = 4;
-// gsap.config({
-//   autoSleep: 60,
-//   force3D: false,
-//   nullTargetWarn: false,
-//   trialWarn: false,
-//   units: {left: "%", top: "%", rotation: "rad"}
-// });
-// Home Page Animation Starts
 let hero_timeline = gsap.timeline();
 
-// hero_timeline.to("#loading_screen", {
-//   duration : 7,
-//   opacity : 0,
-//   yoyo : true,
-//   // duration: 2
-// });
-hero_timeline.to("#home-page", {
-  opacity: 1,
-  duration: 1,
+hero_timeline.to("#loading_screen", {
+  duration : 5,
+  yoyo : true,
 });
+
+hero_timeline.from(".wavy-text",{
+  x: -100,
+  opacity: 0,
+  stagger: 0.1,
+  duration: 1,
+  ease: 'power4.out',
+},"-=5");
+
+hero_timeline.to("#loading_screen",{
+  opacity : 0
+});
+hero_timeline.to(document.querySelector("header"),{
+  opacity : 1,
+  duration : 1,
+})
+hero_timeline.to("#home-page", {
+  opacity : 1,
+  duration: 1,
+},"-=1");
+
 hero_timeline.from("#hero-title", {
   opacity: 0,
   duration: 1,
   yoyo: true,
   x: -1000,
 });
+
 hero_timeline.from(
   "#hero-subtitle",
   {
@@ -37,6 +123,7 @@ hero_timeline.from(
   },
   "-=1"
 );
+
 hero_timeline.from("#hero-btn", {
   opacity: 0,
   scale: 1.9,
@@ -58,8 +145,6 @@ if (aboutUsContainerRight && aboutUsContainerleft) {
       trigger: "#about-us-container",
       start: "top center",
       end: eval(50 % -100),
-      // end : ()=> `+=${document.querySelector("#about-us-container").offsetHeight - 100}`,
-      markers: true,
       scrub: scrubValue,
     },
   });
@@ -76,8 +161,6 @@ if (aboutUsContainerRight && aboutUsContainerleft) {
         trigger: "#about-us-container",
         start: "top center",
         end: eval(50 % -100),
-        // end : ()=> `+=${document.querySelector("#about-us-container").offsetHeight - 100}`,
-        // markers : true,
         scrub: scrubValue,
       },
     },
@@ -94,9 +177,9 @@ if (cardOne && cardTwo && logo) {
     { title: cardOne, x: -1000 },
     { title: cardTwo, x: 1000 },
   ];
-  console.log("IN - 1");
+  // console.log("IN - 1");
   gsap.utils.toArray(cards).forEach((card) => {
-    console.log("IN - 2", card);
+    // console.log("IN - 2", card);
     gsap.from(card.title, {
       opacity: 0,
       x: card.x,
@@ -107,11 +190,6 @@ if (cardOne && cardTwo && logo) {
         trigger: card.title,
         start: "top center",
         end : eval(50% - 100),
-        // end: () =>
-        //   `+=${
-        //     document.querySelector("#service-section").offsetHeight / 2 - 100
-        //   }`,
-        // markers : true,
         scrub: scrubValue,
       },
     });
@@ -127,7 +205,6 @@ if (cardOne && cardTwo && logo) {
         trigger: logo,
         start: "top center",
         end: () => `+=${logo.offsetHeight}`,
-        // markers : true,
         scrub: scrubValue,
       },
     },
@@ -146,103 +223,10 @@ if (cardOne && cardTwo && logo) {
         trigger : contactFormSection,
         start : "-40% 20%",
         end : "+=40",
-        markers : true,
         scrub : scrubValue,
-        // end : eval(50% - 100),
       }
     })
     
   }
-  // let contactUs = gsap.timeline({
-  //   scrollTrigger : {
-  //     trigger : "#contact-us-section",
-  //     markers : true,
-  //     start : "-40% 20%",
-  //     end : "+=40",
-  //     scrub : 1
-  //   }
-  // })
 
-  // contactUs.addLabel("contact-us").from("#contact-form",{
-  //   opacity : 0,
-  //   scale : 1.5,
-  //   yoyo : true,
-  // })
-  // // contact section end
-
-
-  const menubtn = document.getElementById("menubtn");
-  const backToTopBtn = document.getElementById("back-to-top-btn");
-  const contactForm = document.getElementById("contactForm");
-  const ToastContainer = document.getElementById("toast-container");
-
-  window.addEventListener("scroll", () => {
-    const scrollY = window.scrollY;
-    const visibleThreshold = 500;
-
-    if (scrollY > visibleThreshold) {
-      backToTopBtn.style.display = "block";
-    } else {
-      backToTopBtn.style.display = "none";
-    }
-  });
-
-  menubtn.addEventListener("click", () => {
-    const options = document.getElementById("option-li");
-
-    if (options.classList.contains("show-options")) {
-      options.classList.remove("show-options");
-      menubtn.classList.remove("animate-humberger");
-    } else {
-      menubtn.classList.add("animate-humberger");
-      options.classList.add("show-options");
-    }
-  });
-
-  backToTopBtn.addEventListener("click", () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
-  });
-
-  contactForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const name = document.getElementById("name");
-    const email = document.getElementById("email");
-    const phone = document.getElementById("phone");
-    const service = document.getElementById("service");
-    const message = document.getElementById("message");
-
-    console.log(name, email, phone, service, message);
-
-    let Data = new FormData();
-    Data.append("Name", name.value);
-    Data.append("Email", email.value);
-    Data.append("Phone", phone.value);
-    Data.append("ServiceType", service.value);
-    Data.append("Message", message.value);
-    Data.append("Date", new Date());
-
-    fetch(
-      "https://script.google.com/macros/s/AKfycbxz5QbIT2Sfj3ujeJ6wGc8jshB7XvQMdnQlTnTeC0Ca1Y2mjbcFjPRx4HVMXHuJteBO/exec",
-      {
-        method: "POST",
-        body: Data,
-      }
-    ).then((res) => {
-      if (res.ok) {
-        name.value = "";
-        email.value = "";
-        phone.value = "";
-        service.value = "Repair_Service";
-        message.value = "";
-
-        ToastContainer.style.display = "block";
-        setTimeout(() => {
-          ToastContainer.style.display = "none";
-        }, 3000);
-      }
-    });
-  });
 }
--- js/service.js
+++ js/service.js
@@ -1,93 +1,130 @@
-
 // import { RepairingService , DesignService} from "../utils/data";
 
 const RepairingService = {
-    title : "Diamond Machinery Part Repairing",
-    services : [
+  title: "Diamond Machinery Part Repairing",
+  services: [
     {
-        title: "Precision Restord Ball Screw Repairing  Solutions",
-        content: "Trust our experts to restore the precision and functionality of your diamond machinery's ball screws, ensuring smooth operation and enhanced performance.",
-        cardImage : ["../assets/gallery/photo1.png" , "../assets/gallery/photo1.png"],
+      title: "Precision Restord Ball Screw Repairing  Solutions",
+      content:
+        "Trust our experts to restore the precision and functionality of your diamond machinery's ball screws, ensuring smooth operation and enhanced performance.",
+      cardImage: [
+        "../assets/gallery/photo1.png",
+        "../assets/gallery/photo1.png",
+      ],
     },
     {
-        title: "Enhanced Cutting Precision: Sarine Machine Motor Stage Tilting",
-        content: "Our skilled technicians provide specialized services for shearing machine motor stage tilting, enabling enhanced cutting precision for your operations.",
-        cardImage : ["../assets/gallery/photo1.png" , "../assets/gallery/photo1.png"],
-    },{
-        title : "Sarine Machine Motor Repairing",
-        content : "Ensure the reliability and efficiency of your shearing machine's motor with our expert repair solutions, tailored to meet your specific requirements",
-        cardImage : ["../assets/gallery/photo1.png" , "../assets/gallery/photo1.png"],
+      title: "Enhanced Cutting Precision: Sarine Machine Motor Stage Tilting",
+      content:
+        "Our skilled technicians provide specialized services for shearing machine motor stage tilting, enabling enhanced cutting precision for your operations.",
+      cardImage: [
+        "../assets/gallery/photo1.png",
+        "../assets/gallery/photo1.png",
+      ],
     },
     {
-        title : "Precision Restord Ball Screw Repairing  Solutions",
-        content : "Trust our experts to restore the precision and functionality of your diamond machinery's ball screws, ensuring smooth operation and enhanced performance.",
-        cardImage : ["../assets/gallery/photo1.png" , "../assets/gallery/photo1.png"],
+      title: "Sarine Machine Motor Repairing",
+      content:
+        "Ensure the reliability and efficiency of your shearing machine's motor with our expert repair solutions, tailored to meet your specific requirements",
+      cardImage: [
+        "../assets/gallery/photo1.png",
+        "../assets/gallery/photo1.png",
+      ],
     },
     {
-        title : "Precision Restord Ball Screw Repairing  Solutions",
-        content : "Trust our experts to restore the precision and functionality of your diamond machinery's ball screws, ensuring smooth operation and enhanced performance.",
-        cardImage : ["../assets/gallery/photo1.png" , "../assets/gallery/photo1.png"],
+      title: "Precision Restord Ball Screw Repairing  Solutions",
+      content:
+        "Trust our experts to restore the precision and functionality of your diamond machinery's ball screws, ensuring smooth operation and enhanced performance.",
+      cardImage: [
+        "../assets/gallery/photo1.png",
+        "../assets/gallery/photo1.png",
+      ],
     },
     {
-        title : "Precision Restord Ball Screw Repairing  Solutions",
-        content : "Trust our experts to restore the precision and functionality of your diamond machinery's ball screws, ensuring smooth operation and enhanced performance.",
-        cardImage : ["../assets/gallery/photo1.png" , "../assets/gallery/photo1.png"],
-    }],
-    
-}
+      title: "Precision Restord Ball Screw Repairing  Solutions",
+      content:
+        "Trust our experts to restore the precision and functionality of your diamond machinery's ball screws, ensuring smooth operation and enhanced performance.",
+      cardImage: [
+        "../assets/gallery/photo1.png",
+        "../assets/gallery/photo1.png",
+      ],
+    },
+    {
+      title: "Precision Restord Ball Screw Repairing  Solutions",
+      content:
+        "Trust our experts to restore the precision and functionality of your diamond machinery's ball screws, ensuring smooth operation and enhanced performance.",
+      cardImage: [
+        "../assets/gallery/photo1.png",
+        "../assets/gallery/photo1.png",
+      ],
+    },
+  ],
+};
 
 const DesignService = {
-    title : "Design Solution",
-    services : [
+  title: "Design Solution",
+  services: [
     {
-        title : "Transform Ideas into Visual Masterpieces: 2D Design Services",
-        content : "Our expert 2D design services help you transform your ideas into captivating visual masterpieces, tailored to your specific requirements.",
-        cardImage : ["../assets/gallery/photo1.png" , "../assets/gallery/photo1.png"]
+      title: "Transform Ideas into Visual Masterpieces: 2D Design Services",
+      content:
+        "Our expert 2D design services help you transform your ideas into captivating visual masterpieces, tailored to your specific requirements.",
+      cardImage: [
+        "../assets/gallery/photo1.png",
+        "../assets/gallery/photo1.png",
+      ],
     },
     {
-        title : "Immersive 3D Design Experience: Unleash Creativity with 3D Design",
-        content : "Immerse yourself in the world of three-dimensional design as we bring your vision to life with our cutting-edge 3D design solutions.",
-        cardImage : ["../assets/gallery/photo1.png" , "../assets/gallery/photo1.png"]
+      title:
+        "Immersive 3D Design Experience: Unleash Creativity with 3D Design",
+      content:
+        "Immerse yourself in the world of three-dimensional design as we bring your vision to life with our cutting-edge 3D design solutions.",
+      cardImage: [
+        "../assets/gallery/photo1.png",
+        "../assets/gallery/photo1.png",
+      ],
     },
     {
-        title : "Precision and Durability: Unleash the Potential of Sheet Metal",
-        content : "Unlock the potential of sheet metal with our precision-crafted designs that ensure durability and superior performance",
-        cardImage : ["../assets/gallery/photo1.png" , "../assets/gallery/photo1.png"]
+      title: "Precision and Durability: Unleash the Potential of Sheet Metal",
+      content:
+        "Unlock the potential of sheet metal with our precision-crafted designs that ensure durability and superior performance",
+      cardImage: [
+        "../assets/gallery/photo1.png",
+        "../assets/gallery/photo1.png",
+      ],
     },
     {
-        title : "Uncover New Possibilities: Reverse Engineering Solutions",
-        content : "Harness the power of reverse engineering to uncover new possibilities and breathe new life into your existing components.",
-        cardImage : ["../assets/gallery/photo1.png" , "../assets/gallery/photo1.png"]
-    }
-],
+      title: "Uncover New Possibilities: Reverse Engineering Solutions",
+      content:
+        "Harness the power of reverse engineering to uncover new possibilities and breathe new life into your existing components.",
+      cardImage: [
+        "../assets/gallery/photo1.png",
+        "../assets/gallery/photo1.png",
+      ],
+    },
+  ],
+};
 
-}
+const getRepairHolder = () => {
+  let repairHolder = document.getElementById("repair-holder");
 
-const getRepairHolder = ()=>{
-    let repairHolder = document.getElementById("repair-holder")
+  let cardHolderParent = document.createElement("div");
+  cardHolderParent.classList.add("container");
+  cardHolderParent.classList.add("service-cards-holder");
 
-    let cardHolderParent = document.createElement("div")
-    cardHolderParent.classList.add("container") 
-    cardHolderParent.classList.add("service-cards-holder")
-    
-    let holderTitle = document.createElement("div")
-    holderTitle.innerText = RepairingService.title
-    holderTitle.classList.add("text-center")
-    holderTitle.classList.add("serive-name")
+  let holderTitle = document.createElement("div");
+  holderTitle.innerText = RepairingService.title;
+  holderTitle.classList.add("text-center");
+  holderTitle.classList.add("serive-name");
 
-    cardHolderParent.appendChild(holderTitle)
+  cardHolderParent.appendChild(holderTitle);
 
-    let cardHolder = document.createElement("div")
-    cardHolder.classList.add("service-holder")
+  let cardHolder = document.createElement("div");
+  cardHolder.classList.add("service-holder");
 
-    RepairingService.services.forEach((service)=>{
-
-        const images = service.cardImage.map((image)=>{
-            return(
-                `<img src=${image} class="image"/>`
-            )
-        })
-        const card = `
+  RepairingService.services.forEach((service) => {
+    const images = service.cardImage.map((image) => {
+      return `<img src=${image} class="image"/>`;
+    });
+    const card = `
         <div class="d-flex align-items-start service-page-card" id="service-card">
             <img src="../assets/Icons/arrow-right.svg" alt="" class="icon"/>
             <div class="paragraph-holder">
@@ -95,42 +132,42 @@ const getRepairHolder = ()=>{
                     <p class="title">${service.title}</p>
                     <p class="content">${service.content}</p>
                 </div>
-                <div class="d-flex align-items-center image-arr-container" id="image-arr-container">${images}</div>
+                <div class="d-flex align-items-center image-arr-container" id="image-arr-container">
+                <div>${images}</div>
+                <a href="./gallery.html" class="more-btn">More</a>
+                </div>
             </div>
         </div>
-        `
+        `;
 
-        cardHolder.innerHTML += card
-    })
-    cardHolderParent.appendChild(cardHolder)
-    repairHolder.appendChild(cardHolderParent)
-} 
+    cardHolder.innerHTML += card;
+  });
+  cardHolderParent.appendChild(cardHolder);
+  repairHolder.appendChild(cardHolderParent);
+};
 
-const getDesignHolder = ()=>{
-    let designHolder = document.getElementById("design-holder")
+const getDesignHolder = () => {
+  let designHolder = document.getElementById("design-holder");
 
-    let cardHolderParent = document.createElement("div")
-    cardHolderParent.classList.add("container") 
-    cardHolderParent.classList.add("service-cards-holder")
-    
-    let holderTitle = document.createElement("div")
-    holderTitle.innerText = DesignService.title
-    holderTitle.classList.add("text-center")
-    holderTitle.classList.add("serive-name")
+  let cardHolderParent = document.createElement("div");
+  cardHolderParent.classList.add("container");
+  cardHolderParent.classList.add("service-cards-holder");
 
-    cardHolderParent.appendChild(holderTitle)
+  let holderTitle = document.createElement("div");
+  holderTitle.innerText = DesignService.title;
+  holderTitle.classList.add("text-center");
+  holderTitle.classList.add("serive-name");
 
-    let cardHolder = document.createElement("div")
-    cardHolder.classList.add("service-holder")
+  cardHolderParent.appendChild(holderTitle);
 
-    DesignService.services.forEach((service)=>{
+  let cardHolder = document.createElement("div");
+  cardHolder.classList.add("service-holder");
 
-        const images = service.cardImage.map((image)=>{
-            return(
-                `<img src=${image} class="image"/>`
-            )
-        })
-        const card = `
+  DesignService.services.forEach((service) => {
+    const images = service.cardImage.map((image) => {
+      return `<img src=${image} class="image"/>`;
+    });
+    const card = `
         <div class="d-flex align-items-start service-page-card">
             <img src="../assets/Icons/arrow-right.svg" alt="" class="icon"/>
             <div class="paragraph-holder">
@@ -138,17 +175,20 @@ const getDesignHolder = ()=>{
                     <p class="title">${service.title}</p>
                     <p class="content">${service.content}</p>
                 </div>
-                <div class="d-flex align-items-center image-arr-container" id="image-arr-container">${images}</div>
+                <div class="d-flex align-items-center image-arr-container" id="image-arr-container">
+                <div>${images}</div>
+                <a href="./gallery.html" class="more-btn">More</a>
+                </div>
+                </div>
             </div>
         </div>
-        `
+        `;
 
-        cardHolder.innerHTML += card
-    })
-    cardHolderParent.appendChild(cardHolder)
-    designHolder.appendChild(cardHolderParent)
+    cardHolder.innerHTML += card;
+  });
+  cardHolderParent.appendChild(cardHolder);
+  designHolder.appendChild(cardHolderParent);
+};
 
-}
-
-getRepairHolder()
-getDesignHolder()
+getRepairHolder();
+getDesignHolder();
